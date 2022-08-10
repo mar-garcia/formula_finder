046e5f9f25858b7c49f081592279326067fc623a
Mar Garcia Aloy <mar.garcia.aloy@gmail.com>
2022-08-10 09:43:32 +0200

    add ppm calculation

--- app.R
+++ app.R
@@ -17,8 +17,27 @@ ui <- navbarPage(
         fluidRow(column(8, verbatimTextOutput("neutral"))),
         fluidRow(column(8, verbatimTextOutput("pos"))),
         fluidRow(column(8, verbatimTextOutput("neg")))
-      )
-    )
+      )),
+    hr(),
+    sidebarLayout(
+      sidebarPanel(
+        fluidRow(
+          column(4,
+                 numericInput(
+                   inputId = "ppmt",
+                   label = "theoretical m/z:",
+                   value = 166.0862,
+                   step = 0.0001)),
+          column(4,
+                 numericInput(
+                   inputId = "ppme",
+                   label = "experimental m/z value:",
+                   value = 166.0872,
+                   step = 0.0001))
+    )),
+    mainPanel(
+      fluidRow(column(4, verbatimTextOutput("ppm")))
+    ))
     ))
 server <- function(input, output){
   
@@ -36,6 +55,11 @@ server <- function(input, output){
     mzneutral <- calculateMass(input$formula)
     unlist(mass2mz(mzneutral, 
                    adduct = c("[M-H]-", "[M+Cl]-", "[M+CHO2]-", "[2M-H]-")))
+  })
+  
+  output$ppm <- renderPrint({
+    paste(sprintf("%.2f", round(((input$ppme - input$ppmt)/input$ppmt)*1000000, 
+                                2)), "ppm")
   })
 }
 
